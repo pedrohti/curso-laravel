fd0c854d514e081ee4b3235d175f9f40315f2737
Pedro Martins de Moura <pedrommoura1996@gmail.com>
2018-11-05 21:15:14 -0200

    Update do template

--- 2-template-basico.blade.php
+++ 2-template-basico.blade.php
@@ -20,10 +20,54 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <main class="py-4">
-           
-        </main>
-    </div>
+<div id="app">
+    <main class="py-4">
+        <div class="container">
+            <div class="card">
+                <div class="card-header">Cadastro de Visitante</div>
+                <div class="card-body">
+                    <form method="post" action="">
+                        <div class="form-group row">
+                            <div class="col-md-4 text-right">Nome:</div>
+                            <div class="col-md-4">
+                                <input class="form-control" type="text" name="nome" maxlength="255">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-4 text-right">E-mail:</div>
+                            <div class="col-md-4">
+                                <input class="form-control" type="text" name="e-mail" maxlength="60">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-4 text-right">Data de Nascimento:</div>
+                            <div class="col-md-4">
+                                <input class="form-control" type="date" name="date">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-4 text-right">Telefone:</div>
+                            <div class="col-md-4">
+                                <input class="form-control" type="text" name="telefone" maxlength="13">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-4 text-right">Celular:</div>
+                            <div class="col-md-4">
+                                <input class="form-control" type="text" name="celular" maxlength="14">
+                            </div>
+                        </div>
+                        <div class="row align-items-md-center">
+                            <div class="col-md-4 ml-auto mr-auto">
+                                <button class="btn btn-primary" type="submit">Enviar</button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
 </body>
 </html>
